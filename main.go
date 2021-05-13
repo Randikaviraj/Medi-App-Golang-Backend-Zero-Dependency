a7eccf5999f3122133749ccd1d6d26ad636da0a7
Randikaviraj <randikavirajmax@gmail.com>
2021-05-13 05:50:04 +0530

    initial setting of server

--- main.go
+++ main.go
@@ -11,12 +11,20 @@ import (
 )
 
 func main() {
+	// safe server close function
 	serverCloseHandler()
+
 	portnum:=8080
+	// check for the command line argument of port number
 	if  num, err := strconv.Atoi(os.Args[1]); err == nil{
 		portnum=num
 	}
 	log.Printf("Going to listen on port %d\n", portnum)
+
+	// routes
+	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+
+	})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic("Error occured in server----->>" + err.Error())
