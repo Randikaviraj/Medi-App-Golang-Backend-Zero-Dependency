220263361de58c27f5bdc44462027405f6a34bbc
Randikaviraj <randikavirajmax@gmail.com>
2021-05-15 01:00:17 +0530

    controller func added

--- controllers/hospitalControllers.go
+++ controllers/hospitalControllers.go
@@ -7,29 +7,32 @@ import (
 	"github.com/randika/Medi-App-Golang-Backend/database"
 )
 
-type doctorName struct{
-	name string
+type hospital struct{
+
+	hospitalName string
 }
 
-type speciality struct{
-	specializedField string
+type date struct{
+	hospital
+	date string
 }
 
-func FindByNameController(w http.ResponseWriter,r *http.Request){
+func FindByHospitalController(w http.ResponseWriter,r *http.Request){
 
-	var spe speciality
+	var hos hospital
 
     // Try to decode the request body into the struct. If there is an error,
     // respond to the client with the error message and a 400 status code.
-	err := json.NewDecoder(r.Body).Decode(&spe)
+	err := json.NewDecoder(r.Body).Decode(&hos)
 	
     if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
 
-	var data []database.Speciality
-	if err:=database.QueryBySpecialityName(spe.specializedField,&data); err!=nil {
+	var data []database.Hospital
+
+	if err:=database.QueryByHospitalName(hos.hospitalName,&data);err!=nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
         return
 	}
@@ -46,24 +49,27 @@ func FindByNameController(w http.ResponseWriter,r *http.Request){
 	w.Write(jsonData)
 }
 
-func FindBySpecilityController(w http.ResponseWriter,r *http.Request){
 
-	var doc doctorName
+func FindByDateController(w http.ResponseWriter,r *http.Request){
+
+	var date date
 
     // Try to decode the request body into the struct. If there is an error,
     // respond to the client with the error message and a 400 status code.
-	err := json.NewDecoder(r.Body).Decode(&doc)
+	err := json.NewDecoder(r.Body).Decode(&date)
 	
     if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
 
-	var data []database.HospitalAndDoctor
-	if err:=database.QueryByDoctorName(doc.name,&data);err!=nil {
+	var data []database.Doctor
+
+	if err:=database.QueryDoctorByDate(date.hospitalName,date.date,&data);err!=nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
         return
 	}
+
 	jsonData,err:=json.Marshal(data)
 
 	if err!=nil {
