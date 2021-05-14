315c1884e5e5c34c65b8a729035823e53cd1fc8f
Randikaviraj <randikavirajmax@gmail.com>
2021-05-14 22:11:31 +0530

    mysql connector added

--- database/connection.go
+++ database/connection.go
@@ -3,17 +3,26 @@ package database
 import (
 	"database/sql"
 	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var Db *sql.DB
 
 func CreateDbConnection(dataSourceName string)  {
-	db,err:=sql.Open("sql",dataSourceName)
 
-	if err==nil {
-		log.Fatal(err)
+	db,err:=sql.Open("mysql",dataSourceName)
+	
+	if err!=nil {
+		log.Fatal(err.Error())
 		panic("Error occured in CreateDbConnection :")
 	}
+
+	db.SetConnMaxLifetime(time.Second * 5)
+	db.SetMaxIdleConns(0)
+	db.SetMaxOpenConns(151)
+
 
 	Db=db
 }
